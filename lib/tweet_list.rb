c5402d2ace793fd04bd62ac00800a4837a823b77
Jurgens du Toit <jrgns@jadeit.co.za>
2015-11-06 08:49:39 +0200

    fix: Allow spaces in the username

--- lib/tweet_list.rb
+++ lib/tweet_list.rb
@@ -7,7 +7,7 @@ class TweetList < FileList
   end
 
   def parse(line)
-    matches = line.strip.match(/^(\S+)> ?(.*)/)
+    matches = line.chomp.match(/^([^>]+)> ?(.*)/)
     return if matches.nil?
     store << { user: matches[1], tweet: matches[2][0..140] }
   end
