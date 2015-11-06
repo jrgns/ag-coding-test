6a0f872a048ef2120d791a1dd76e13f1fb1bb559
Jurgens du Toit <jrgns@jadeit.co.za>
2015-11-06 08:45:26 +0200

    chore: Add tests to ensure tweet length and allow spaces in usernames

--- spec/tweet_list_spec.rb
+++ spec/tweet_list_spec.rb
@@ -15,5 +15,18 @@ describe TweetList do
       expect(subject.store[1]).to eq({user: 'Ward', tweet: 'There are only two hard things in Computer Science: cache invalidation, naming things and off-by-1 errors.'})
       expect(subject.store[2]).to eq({user: 'Alan', tweet: 'Random numbers should not be generated with a method chosen at random.'})
     end
+
+    it 'allows for spaces in usernames' do
+      list = TweetList.new(StringIO.new('Spaces are Cool> I like spaces     '))
+      expect(list.store[0]). to eq({user: 'Spaces are Cool', tweet: 'I like spaces     '})
+    end
+
+    it 'restricts tweets to 140 characters' do
+      tweet = Array.new(4, 'This line is longer than 140 characters.').join(' ')
+      line = 'Mr Verbose> ' + tweet
+      list = TweetList.new(StringIO.new(line))
+      expect(list.store[0][:tweet].length).to be(140)
+    end
   end
 end
+
